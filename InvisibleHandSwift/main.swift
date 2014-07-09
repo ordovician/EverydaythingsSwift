d45aa2e0787b9bd59807610212681b143867c1eb
Erik Engheim <erik.engheim@mac.com>
2014-07-09 10:19:12 +0200

    Initial Commit

---
+++ InvisibleHandSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  InvisibleHandSwift
+//
+//  Created by Erik Engheim on 7/9/14.
+//  Copyright (c) 2014 Erik Engheim. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
